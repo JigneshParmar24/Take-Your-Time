41806fc2ca7d91798fef7eb9aba697b55ef4716c
Jignesh Pamar <jigneshparmar1024@gmail.com>
2025-07-23 09:33:00 +0530

    basic to do

---
+++ client/src/components/TaskList.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import dayjs from "dayjs";
+import './TaskList.css'
+
+const TaskList = () => {
+
+    const [newTask, setNewTask] = useState("")
+    const [list, setList] = useState([])
+
+    function handleTitleChange(e) {
+        setNewTask(e.target.value);
+    }
+
+    function createTask(title) {
+        return {
+            title: title,
+            isComplete: false,
+            date: dayjs().format('YYYY-MM-DD hh:mm:ss A'),
+            id: uuidv4()
+        };
+    }
+
+    const AddInDaList = () => {
+        if(newTask.trim() === "") {return}
+        const obj = createTask(newTask);
+        setList([...list, obj]);
+        setNewTask("");
+    }
+
+    const DeleteInDaList = (index) => {
+        setList(list.filter((_, i) => i !== index))
+    }
+
+    const EditDaList = (index) => {
+        setNewTask(list[index].title)
+        DeleteInDaList(index);
+    }
+
+    const isDone = (i) => {
+        let newList = list.map((item, index) => {
+            if (index === i) {
+                return { ...item, isComplete: !item.isComplete }
+            }
+            return item
+        })
+        setList(newList);
+    }
+
+    return (
+        <>
+            <div className='inputContainer'>
+                <input type='text' value={newTask}
+                    onChange={handleTitleChange} placeholder='Enter Da Task' className='taskInput'>
+                </input>
+                <button className="addbtn" onClick={AddInDaList}> ADD </button>
+            </div>
+
+            <div className="taskContainer">
+                <ul>
+                    {list.map((item, index) => {
+                        return (
+                            <li key={item.id}>
+                                <div className={item.isComplete ? 'taskDisplay strike' : 'taskDisplay'} onClick={() => isDone(index)}>
+                                    {item.title} - {item.date}
+                                </div>
+
+                                <button className='deletebtn' onClick={() => DeleteInDaList(index)}>delete</button>
+                                <button className='editbtn' onClick={() => EditDaList(index)}>edit</button>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+
+        </>
+    )
+}
+
+export default TaskList
